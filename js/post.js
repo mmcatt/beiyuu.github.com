562b5e416374f8c68c3789c7f31f0ecb94223a50
Cynthia <mmcatt@qq.com>
2014-06-17 18:01:34 +0800

    a

--- js/post.js
+++ js/post.js
@@ -24,19 +24,6 @@ $(document).ready(function(){
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
 
 
-    $('.entry a').each(function(index,element){
-        var href = $(this).attr('href');
-        if(href){
-            if(href.indexOf('#') == 0){
-            }else if ( href.indexOf('/') == 0 || href.toLowerCase().indexOf('beiyuu.com')>-1 ){
-            }else if ($(element).has('img').length){
-            }else{
-                $(this).attr('target','_blank');
-                $(this).addClass('external');
-            }
-        }
-    });
-
     (function(){
         var ie6 = ($.browser.msie && $.browser.version=="6.0") ? true : false;
 
